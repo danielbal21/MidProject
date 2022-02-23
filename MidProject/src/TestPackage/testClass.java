c77f602ef115eff124804924d75dfa4a990afe1f
Daniel-PC <Daniel-PC@Daniel>
2022-02-23 20:24:20 +0200

    Change on master

--- MidProject/src/TestPackage/testClass.java
+++ MidProject/src/TestPackage/testClass.java
@@ -3,5 +3,6 @@ package TestPackage;
 public class testClass {
 	public static void main(String[] args) {
 		System.out.println("tesTX");
+		System.out.println("Change on Laptop Master Branch");
 	}
 }
