ef9b906c885928106bb0b5377c54da746aa0f736
danielbal21 <daniel.bal0797@gmail.com>
2022-03-02 17:40:24 +0200

    [Daniel] update

--- MidProject/src/TestPackage/testClass.java
+++ MidProject/src/TestPackage/testClass.java
@@ -5,10 +5,7 @@ public class testClass {
 		System.out.println("tesTX");
 		System.out.println("Change on Laptop Master Branch");
 		System.out.println("Daniel PC Branch");
-<<<<<<< HEAD
 		System.out.println("Added on my personal branch daniel");
-=======
 		System.out.println("Group change - daniel");
->>>>>>> refs/heads/master
 	}
 }
