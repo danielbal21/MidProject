06ccd08b6118a26cb256f59b9b9373c58f53dcd7
danielbal21 <daniel.bal0797@gmail.com>
2022-03-02 17:38:25 +0200

    [Daniel] - my branch

--- MidProject/src/TestPackage/testClass.java
+++ MidProject/src/TestPackage/testClass.java
@@ -4,5 +4,6 @@ public class testClass {
 	public static void main(String[] args) {
 		System.out.println("tesTX");
 		System.out.println("Daniel PC Branch");
+		System.out.println("Added on my personal branch daniel");
 	}
 }
