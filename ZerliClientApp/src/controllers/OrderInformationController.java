9ba2124aae5ca42656866d06847e75efc4702dda
danielbal21 <daniel.bal0797@gmail.com>
2022-05-09 20:14:04 +0300

    [Daniel] Starting work Payment and Order info

--- ZerliClientApp/src/controllers/OrderInformationController.java
+++ ZerliClientApp/src/controllers/OrderInformationController.java
@@ -7,7 +7,9 @@ import client.ClientApp;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -15,10 +17,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.AnchorPane;
 
 public class OrderInformationController implements UserControl {
 
+	boolean unsafeExit = false;
+	
 	public void loadBranches()
 	{
 		ClientApp.ProtocolHandler.Invoke(RequestType.GetBranches, null, null, true);
@@ -58,6 +64,7 @@ public class OrderInformationController implements UserControl {
     void nextBtn_Click(ActionEvent event) {
     	if(!isInputValid()) return;
     	
+    	LoginController.windowControl.putPipe("orderInfo",
     		
     }
     
@@ -103,7 +110,17 @@ public class OrderInformationController implements UserControl {
 
 	@Override
 	public void onExit() {
-		// TODO Auto-generated method stub
+		/*if(unsafeExit)
+		{
+			Alert confirmAlert = new Alert(AlertType.CONFIRMATION);
+			confirmAlert.setTitle("Leaving Order Information");
+			confirmAlert.setContentText("Are you sure you want to cancel the order?");
+			ButtonType yes = new ButtonType("Yes", ButtonData.YES);
+			ButtonType no = new ButtonType("No",ButtonData.NO);
+			confirmAlert.getDialogPane().getButtonTypes().add(yes);
+			confirmAlert.getDialogPane().getButtonTypes().add(no);
+			confirmAlert.showAndWait();
+		}*/
 		
 	}
 
